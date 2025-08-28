4cf13efb543c72c5db49ea68238c15a5fe4e8fe3
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-08-28 19:36:51 +0200

    Move metadata and home page to top of studio structure

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -5,16 +5,18 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title("Blog")
     .items([
-      S.documentTypeListItem("post").title("Posts"),
-      S.documentTypeListItem("category").title("Categories"),
-      S.documentTypeListItem("author").title("Authors"),
-      S.divider(),
       S.listItem()
         .title("Metadata")
         .child(S.document().schemaType("metadata").documentId("metadata")),
       S.listItem()
         .title("Home page")
         .child(S.document().schemaType("homePage").documentId("homePage")),
+
+      S.divider(),
+
+      S.documentTypeListItem("post").title("Posts"),
+      S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("author").title("Authors"),
 
       ...S.documentTypeListItems().filter(
         (item) =>
