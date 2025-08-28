7fa317fa292aee4dfb9d418943b8d09dc12c4b67
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-08-28 08:24:41 +0200

    Add guiding content instead of empty fields after initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,26 +14,46 @@ export default async function Home() {
   return (
     <div className="space-y-10">
       <div>
-        <RichContent value={homePage.hero} />
+        {!homePage.hero ? (
+          <RichContent value={homePage.hero} />
+        ) : (
+          <div className="space-y-2">
+            <p>
+              No <code>hero</code> content found in the CMS. Edit the &quot;Home
+              page&quot; document in your Sanity studio.
+            </p>
+            <p>
+              To access the studio, follow the instructions in the{" "}
+              <i>README.md</i>.
+            </p>
+          </div>
+        )}
       </div>
 
-      <ol className="list-none space-y-4">
-        {posts.map((post) => (
-          <li key={post.slug.current}>
-            <Link href={`/post/${post.slug.current}`}>
-              <span className="font-bold underline">{post.title}</span>
-              <br />
-              <span className="text-sm text-secondary">
-                {post.author?.name && `${post.author?.name} `}(
-                <time dateTime={post.publishedAt}>
-                  {new Date(post.publishedAt).toDateString()}
-                </time>
-                )
-              </span>
-            </Link>
-          </li>
-        ))}
-      </ol>
+      <div className="space-y-4">
+        <h2 className="text-2xl font-bold">Latest Posts</h2>
+        {!posts.length ? (
+          <ol className="list-none space-y-4">
+            {posts.map((post) => (
+              <li key={post.slug.current}>
+                <Link href={`/post/${post.slug.current}`}>
+                  <span className="font-bold underline">{post.title}</span>
+                  <br />
+                  <span className="text-sm text-secondary">
+                    {post.author?.name && `${post.author?.name} `}(
+                    <time dateTime={post.publishedAt}>
+                      {new Date(post.publishedAt).toDateString()}
+                    </time>
+                    )
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <p>No posts found. You can create posts in the studio.</p>
+        )}
+      </div>
     </div>
   );
 }
