d5bcaf512b6922ca89cb5ef5d8c474ce4d06036b
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-08-29 10:41:21 +0200

    Add link to source code in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,9 +4,13 @@ export function Footer() {
   return (
     <footer className="bg-background border-t border-muted text-secondary text-sm">
       <div className="max-w-3xl mx-auto p-4">
-        Made by{" "}
+        made by{" "}
         <ExternalLink href="https://arnaudrenaud.com" className="font-normal">
           Arnaud Renaud
+        </ExternalLink>{" "}
+        • codebase:{" "}
+        <ExternalLink href="https://github.com/arnaudrenaud/ssg-nextjs-cms-sanity">
+          github.com/arnaudrenaud/ssg-nextjs-cms-sanity
         </ExternalLink>
       </div>
     </footer>
